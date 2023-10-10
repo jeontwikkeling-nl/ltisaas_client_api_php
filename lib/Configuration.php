8b610a35767a00dd4c72213f64862806532d7142
Luuk Verhoeven <luuk@ldesign.nl>
2023-10-10 21:47:51 +0200

    Update to version V1.0.33

--- lib/Configuration.php
+++ lib/Configuration.php
@@ -100,7 +100,7 @@ class Configuration
      *
      * @var string
      */
-    protected $userAgent = 'LTISaasClient V1.0.32';
+    protected $userAgent = 'LTISaasClient V1.0.33';
 
     /**
      * Debug switch (default set to false)
