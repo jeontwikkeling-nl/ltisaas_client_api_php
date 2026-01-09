df00481e68ed67518977704d51bc8b751d481aad
Luuk Verhoeven <luuk@ldesign.nl>
2026-01-09 10:16:50 +0100

    Update to version V1.0.52

--- lib/Configuration.php
+++ lib/Configuration.php
@@ -100,7 +100,7 @@ class Configuration
      *
      * @var string
      */
-    protected $userAgent = 'LTISaasClient V1.0.51';
+    protected $userAgent = 'LTISaasClient V1.0.52';
 
     /**
      * Debug switch (default set to false)
